111b1104ce21baaf9dcd7dbe2e097fe991a87129
Faikamajid <72315835+Faikamajid@users.noreply.github.com>
2021-10-08 18:21:43 +0530

    Create App.controller.js

---
+++ App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define([
+    'sap/ui/core/mvc/Controller'  
+], function(Controller) {
+    'use strict';
+    return Controller.extend("aprdemo.controller.App",
+    {
+        onClick:function()
+            {
+          alert("welcome to trainings");
+            }
+        
+    });
+    
+});
